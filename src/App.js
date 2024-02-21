7debc434ea4a15b4f25f549f0bb53520b5b342be
Isha kendulkar <kendulkarisha@gmail.com>
2024-02-21 12:41:29 +0530

    delete functionality added

--- src/App.js
+++ src/App.js
@@ -2,22 +2,59 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
+var count= 1
+
 function App() {
- const [todo, setTodo] = useState([])
+ var [todo, setTodo] = useState([])
   const addtodo = () =>
   {
     console.log("Add todo")
 
  const todotext = document.getElementById("todoInput").value
+ let todoObject= {
+  id: count++,
+  title: todotext
+ }
 
  console.log("todotext: "+todotext)
 
- todo.push(todotext)
+ todo.push(todoObject)
  setTodo([...todo]) //=>
     //todoArray.puch()
   }
 
-  console.log("Length of Todo",todo.length)
+  // console.log("Length of Todo",todo.length)
+
+  const getDynamicList = () =>
+  {
+    let response =""
+    for(let count1 = 0; count1<todo.length; count1++)
+    {
+      console.log("Todo[count].title: "+todo[count1].title)
+      response += <div>{todo[count1].title}</div>
+      console.log(response)
+    }
+    console.log(response)
+    return response
+  }
+
+   const deletetodo = (id) =>
+   {
+    console.log("Delete todo id:", id)
+    todo = todo.filter(todoTemp => {
+      if(id === todoTemp.id)
+      {
+        return false
+      }
+      else{
+        return  true
+      }
+    })
+    setTodo([...todo])
+
+    
+
+   }
 
   return (
     <div>
@@ -27,7 +64,10 @@ function App() {
       {todo.map(tempTodo =>
         {
       
-       return <div>{tempTodo}</div>
+       return <div>
+        {tempTodo.title}
+        <button onClick={()=>deletetodo()}>Delete</button>
+        </div>
 
       })}
     </div>
